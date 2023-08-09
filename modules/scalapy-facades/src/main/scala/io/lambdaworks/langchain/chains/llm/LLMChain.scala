0905d2ad91f5af9b11a87ab62119a7503cb2c990
mvelimir <velimirmilinkovic1@gmail.com>
2023-08-09 17:58:55 +0200

    Organize imports

--- modules/scalapy-facades/src/main/scala/io/lambdaworks/langchain/chains/llm/LLMChain.scala
+++ modules/scalapy-facades/src/main/scala/io/lambdaworks/langchain/chains/llm/LLMChain.scala
@@ -1,9 +1,9 @@
 package io.lambdaworks.langchain.chains.llm
 
 import io.lambdaworks.langchain.chains.base.Chain
-import io.lambdaworks.langchain.schema.prompttemplate.BasePromptTemplate
 import io.lambdaworks.langchain.schema.languagemodel.BaseLanguageModel
 import io.lambdaworks.langchain.schema.outputparser.BaseLLMOutputParser
+import io.lambdaworks.langchain.schema.prompttemplate.BasePromptTemplate
 import me.shadaj.scalapy.py
 
 @py.native
