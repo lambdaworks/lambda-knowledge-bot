d8b116af11c0299368e1d6a760b8fb83583fdb48
Ivana Kasikovic <ikasikovic@yahoo.com>
2024-01-23 15:10:56 +0100

    Add logo

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { SidebarMobile } from './sidebar-mobile'
 import { ChatHistory } from './chat-history'
 import { Chat } from '@/lib/types';
+import Logo from '../assets/knowle-dark-bckrnd.svg'
 
 export function Header() {
   const chats: Chat[] = [];
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
+      <span>Knowle</span>
       <div className="flex items-center">
         <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
           <SidebarMobile>
@@ -14,8 +16,8 @@ export function Header() {
           </SidebarMobile>
         </React.Suspense>
       </div>
-      <a target='__blank'>
-       Knowle
+      <a target='__blank' className="logo-container">
+        <img src={Logo} alt='Logo'></img>
       </a>
     </header>
   )
