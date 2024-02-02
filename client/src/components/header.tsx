959f27f2007d6eb326f81d7390e5316c2a98eb5d
Ivana Kasikovic <ikasikovic@yahoo.com>
2024-02-02 11:38:04 +0100

    Change header

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -48,6 +48,9 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 flex items-center w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <span>Knowλe</span>
+      <a target='__blank' className="logo-container" style={{ marginLeft: 5 }}>
+        <img src={Logo} alt='Logo'></img>
+      </a>
       <div className="flex items-center">
         <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
           <SidebarMobile>
@@ -61,9 +64,6 @@ export function Header() {
           <UserOrLogin />
         </React.Suspense>
       </div>
-      <a target='__blank' className="logo-container" style={{ marginLeft: 20 }}>
-        <img src={Logo} alt='Logo'></img>
-      </a>
     </header>
   )
 }
