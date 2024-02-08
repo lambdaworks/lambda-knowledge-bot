52563b18935336b81149dd5ab7fb742c30415b1e
katicm <markokatic97@gmail.com>
2024-02-08 12:56:28 +0100

    Remove unneeded root layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,39 @@
-import React from 'react';
-import { Auth0Provider } from '@auth0/auth0-react';
-import IndexPage from '@/pages/chat';
-import RootLayout from '@/layout';
+import React from "react";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { Toaster } from "react-hot-toast";
+
+import IndexPage from "@/pages/chat";
+import { Providers } from "./components/providers";
+import { cn } from "./lib/utils";
+import { Header } from "./components/header";
+
+import "@/index.css";
 
 const auth0Domain = import.meta.env.VITE_DOMAINE;
-const clientId = import.meta.env.VITE_CLIENT_ID
-const url = import.meta.env.VITE_FRONT_URL
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const url = import.meta.env.VITE_FRONT_URL;
 
 const App = (): JSX.Element => {
-
   return (
     <Auth0Provider
       domain={auth0Domain}
       clientId={clientId}
-      authorizationParams={{ redirect_uri: `${url}`}}
+      authorizationParams={{ redirect_uri: `${url}` }}
     >
-      <RootLayout>
-        <IndexPage />
-      </RootLayout>
+      <div className={cn("font-sans antialiased")}>
+        <Toaster />
+        <Providers>
+          <>
+            <div className="flex flex-col min-h-screen">
+              <Header />
+              <main className="flex flex-col flex-1 bg-muted/50">
+                <IndexPage />
+              </main>
+            </div>
+            {/* <TailwindIndicator /> */}
+          </>
+        </Providers>
+      </div>
     </Auth0Provider>
   );
 };
