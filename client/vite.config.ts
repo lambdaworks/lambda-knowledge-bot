fa5ba972dcded520fa913ab569365f81fdcc3216
katicm <markokatic97@gmail.com>
2024-02-09 09:54:55 +0100

    Add server host config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    hmr: {
+      host: "localhost",
     },
   },
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
