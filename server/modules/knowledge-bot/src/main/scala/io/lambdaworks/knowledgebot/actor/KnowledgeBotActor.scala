7b54c497ce91cdda845bdd8047777731f288902d
Milorad Vojnovic <milekuglas@gmail.com>
2024-02-28 16:29:27 +0100

    Fix code remarks

--- server/modules/knowledge-bot/src/main/scala/io/lambdaworks/knowledgebot/actor/KnowledgeBotActor.scala
+++ server/modules/knowledge-bot/src/main/scala/io/lambdaworks/knowledgebot/actor/KnowledgeBotActor.scala
@@ -7,7 +7,6 @@ import akka.stream.scaladsl.Source
 import io.lambdaworks.knowledgebot.Main
 import io.lambdaworks.knowledgebot.actor.KnowledgeBotActor.{
   Event,
-  InactivityTimeout,
   LLMResponse,
   MessageToken,
   NewUserMessage,
@@ -29,7 +28,6 @@ object KnowledgeBotActor {
   final case class NewUserMessage(content: String, replyBack: ActorRef[SessionInfo]) extends Event
   private final case class MessageToken(text: String)                                extends Event
   private final case class LLMResponse(response: Map[String, py.Any])                extends Event
-  private final case object InactivityTimeout                                        extends Event
 
   final case class ResponseData(
     messageToken: String,
@@ -62,23 +60,18 @@ private final class KnowledgeBotActor(
   chatMessageRepository: ChatMessageRepository
 )(implicit val system: ActorSystem[_]) {
   private def acceptEvents(): Behavior[Event] =
-    Behaviors.receiveMessage {
-      case NewUserMessage(content, replyBack) =>
-        val (queue, source) = Source.queue[ServerSentEvent](1).preMaterialize()
+    Behaviors.receiveMessage { case NewUserMessage(content, replyBack) =>
+      val (queue, source) = Source.queue[ServerSentEvent](1).preMaterialize()
 
-        replyBack ! SessionInfo(source, chat.id)
+      replyBack ! SessionInfo(source, chat.id)
 
-        retrieverActor ! LLMRetrieverActor.Request(content)
+      retrieverActor ! LLMRetrieverActor.Request(content)
 
-        val msgId       = UUID.randomUUID().toString
-        val userMessage = UserMessage(msgId, chat.userId, chat.id, DateTime.now(DateTimeZone.UTC), content, None)
-        chatMessageRepository.put(userMessage)
+      val msgId       = UUID.randomUUID().toString
+      val userMessage = UserMessage(msgId, chat.userId, chat.id, DateTime.now(DateTimeZone.UTC), content, None)
+      chatMessageRepository.put(userMessage)
 
-        processMessageTokens(queue)
-      case InactivityTimeout =>
-        routerActor ! MessageRouterActor.SessionExpired(chat.id)
-
-        Behaviors.stopped
+      processMessageTokens(queue)
     }
 
   private def processMessageTokens(
@@ -120,9 +113,6 @@ private final class KnowledgeBotActor(
           relevantDocuments
         )
         chatMessageRepository.put(assistantMessage)
-        acceptEvents()
-      case InactivityTimeout =>
-        routerActor ! MessageRouterActor.SessionExpired(chat.id)
 
         Behaviors.stopped
     }
