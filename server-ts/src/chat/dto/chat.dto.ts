dacc7dc0eb290368f71e19dd482cf311ecdc3131
Mihajlo Maksimovic <mihajlo2000@gmail.com>
2024-03-26 10:54:15 +0100

    Implement swagger API documentation

--- server-ts/src/chat/dto/chat.dto.ts
+++ server-ts/src/chat/dto/chat.dto.ts
@@ -1,21 +1,26 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsString, IsUUID, IsNumberString, IsOptional } from 'class-validator';
 export class CreateChatDto {
   @IsString()
+  @ApiProperty()
   readonly content: string;
 }
 
 export class PaginationDto {
   @IsString()
   @IsOptional()
+  @ApiPropertyOptional()
   readonly lastKey: string;
 
   @IsNumberString()
   @IsOptional()
+  @ApiPropertyOptional()
   readonly limit: string;
 }
 
 export class SentChatIdDto {
   @IsString()
   @IsUUID()
+  @ApiProperty()
   readonly chatId: string;
 }
