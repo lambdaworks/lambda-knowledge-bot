d9c86ca0d1b77080be97abd095ae13879a70c0a7
Mihajlo Maksimovic <94182463+mihajlo-ra92@users.noreply.github.com>
2024-03-26 12:59:31 +0100

    Add swagger documentation (#62)
    * Implement api
    
    * Make endpoints async
    
    * Implement validation
    
    * Implement swagger API documentation
    
    * Cast limit query param to number

--- server-ts/src/app.module.ts
+++ server-ts/src/app.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [ChatModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
--- server-ts/src/chat/dto/chat.dto.ts
+++ server-ts/src/chat/dto/chat.dto.ts
@@ -1,3 +1,30 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsString, IsUUID, IsOptional, IsInt, Min, Max } from 'class-validator';
+
+import { Type } from 'class-transformer';
 export class CreateChatDto {
+  @IsString()
+  @ApiProperty()
   readonly content: string;
+}
+
+export class PaginationDto {
+  @IsString()
+  @IsOptional()
+  @ApiPropertyOptional()
+  readonly lastKey?: string;
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(0)
+  @Max(50)
+  @ApiPropertyOptional()
+  readonly limit?: number;
+}
+
+export class SentChatIdDto {
+  @IsString()
+  @IsUUID()
+  @ApiProperty()
+  readonly chatId: string;
 }
