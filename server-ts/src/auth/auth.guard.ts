f0e6275710b41fc06914b284a26bad8f1d84bfb1
Mihajlo Maksimovic <mihajlo2000@gmail.com>
2024-03-28 13:51:31 +0100

    Remove @nestjs/jwt

--- server-ts/src/auth/auth.guard.ts
+++ server-ts/src/auth/auth.guard.ts
@@ -1,10 +1,8 @@
 import { ExecutionContext, mixin } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 
 export const Auth0Guard = (isMaybe: boolean = false) => {
   class Auth0GuardMixin extends AuthGuard('jwt') {
-    jwtService = new JwtService();
     canActivate(ctx: ExecutionContext) {
       const req = ctx.switchToHttp().getRequest();
       const authHeader = req.headers?.authorization;
